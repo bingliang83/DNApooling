9d5e23d170d060b63fef0ed05d9c17c609e5fe7d
bingliang83 <liangbing1983@hotmail.com>
2025-07-07 00:13:13 +0800

    Initial commit of DNApooling package

---
+++ R/run_analysis.R
@@ -0,0 +1,110 @@
+#' Run Family Contribution Analysis
+#'
+#' This function reads genomic and phenotypic input files, runs DEoptim optimization,
+#' and saves results including allele frequencies and parent contributions.
+#'
+#' @param geno_parents_file Path to geno_parents.txt
+#' @param pheno_parents_file Path to pheno_parents.txt
+#' @param geno_off_file Path to geno_off.txt
+#' @param pheno_off_file Path to pheno_off.txt
+#' @param af_pool_file Path to af_pool.txt
+#' @param out_dir Directory to save result files (default: current directory)
+#' @param maxgen Maximum number of generations (default: 100000)
+#' @param nrep Number of replicates (default: 5)
+#' @param popsize_factor Multiplier to determine population size (default: 10)
+#'
+#' @return Saves result files to out_dir
+#' @export
+run_analysis <- function(geno_parents_file,
+                         pheno_parents_file,
+                         geno_off_file,
+                         pheno_off_file,
+                         af_pool_file,
+                         out_dir = ".",
+                         maxgen = 100000,
+                         nrep = 5,
+                         popsize_factor = 10) {
+  
+  library(DEoptim)
+  library(ggplot2)
+  library(gaston)
+  library(dplyr)
+  
+  # Read input files
+  geno_parents <- read.table(geno_parents_file, stringsAsFactors = FALSE)
+  pheno_parents <- read.table(pheno_parents_file, header = TRUE)
+  geno_off <- read.table(geno_off_file, stringsAsFactors = FALSE)
+  pheno_off <- read.table(pheno_off_file, header = TRUE, stringsAsFactors = FALSE)
+  Mpool <- as.matrix(read.table(af_pool_file, header = FALSE))
+  
+  nsnps <- ncol(geno_parents)
+  np <- 1
+  sample_perpool <- sum(pheno_off$pool == 1)
+  
+  dads <- pheno_parents[pheno_parents$sex == 1, 1]
+  mums <- pheno_parents[pheno_parents$sex == 2, 1]
+  nfam <- length(dads) * length(mums)
+  
+  families_all <- expand.grid(siresim = dads, damsim = mums)
+  M <- matrix(0, ncol = nfam, nrow = nsnps)
+  
+  for(i in 1:nfam) {
+    gen1 <- geno_parents[rownames(geno_parents) == families_all$siresim[i], ]
+    gen2 <- geno_parents[rownames(geno_parents) == families_all$damsim[i], ]
+    M[, i] <- (as.numeric(gen1) + as.numeric(gen2)) / 4
+  }
+  
+  F <- 0.8
+  CR <- 0.5
+  popsize <- popsize_factor * nfam
+  
+  of <- function(x) {
+    v <- matrix(x, ncol = np, nrow = nfam, byrow = TRUE)
+    z <- M %*% v
+    sum(abs(z - Mpool)) + (sum(v) - 1)^2 * 1e3
+  }
+  
+  est_parent_contrib_final <- list()
+  siresim_list <- list()
+  damsim_list <- list()
+  res_list <- list()
+  
+  for(r in 1:nrep) {
+    outDEoptim <- DEoptim(of, lower = rep(0, nfam*np), upper = rep(1, nfam*np),
+                          DEoptim.control(strategy = 1, VTR = 0.5, itermax = maxgen, NP = popsize,
+                                          trace = TRUE, F = F, CR = CR))
+    
+    best_v <- matrix(outDEoptim$optim$bestmem, ncol = np, nrow = nfam, byrow = TRUE)
+    Yest <- M %*% best_v
+    
+    write.table(best_v, file.path(out_dir, paste0("ContribSolutionRepli", r, ".txt")), 
+                quote = FALSE, row.names = FALSE, col.names = FALSE)
+    write.table(Yest, file.path(out_dir, paste0("AlleleFreqSolutionRepli", r, ".txt")), 
+                quote = FALSE, row.names = FALSE, col.names = FALSE)
+    
+    est_contrib <- cbind(families_all, best_v)
+    
+    siresim_list[[r]] <- est_contrib %>%
+      group_by(siresim) %>%
+      summarise(contrib_percent = sum(V1)) %>%
+      rename(parent = siresim) %>%
+      mutate(parent_type = 'sire', replicate = r)
+    
+    damsim_list[[r]] <- est_contrib %>%
+      group_by(damsim) %>%
+      summarise(contrib_percent = sum(V1)) %>%
+      rename(parent = damsim) %>%
+      mutate(parent_type = 'dam', replicate = r)
+    
+    est_parent_contrib_final[[r]] <- rbind(siresim_list[[r]], damsim_list[[r]]) %>% arrange(parent)
+    
+    res_list[[r]] <- data.frame(replicate = r, pop = popsize, gen = maxgen, 
+                                lastiter = min(which(outDEoptim$member$bestvalit == outDEoptim$optim$bestval)))
+  }
+  
+  combined_contrib <- do.call(rbind, est_parent_contrib_final)
+  write.csv(combined_contrib, file.path(out_dir, "est_parent_contrib_final_all.csv"), row.names = FALSE)
+  
+  combined_res <- do.call(rbind, res_list)
+  write.table(combined_res, file.path(out_dir, "result.txt"), quote = FALSE, row.names = FALSE, col.names = TRUE)
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# DNA_pooling
+
+An R package to estimate family contributions in pooled DNA data using DEoptim.
+
+## Installation
+
+```r
+devtools::install_github("bingliang83/DNApooling")
